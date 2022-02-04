fe09788d276bc80d42bb04691a841a7ca442a807
ebayraktaroglu <97638717+ebayraktaroglu@users.noreply.github.com>
2022-02-03 16:36:50 -0800

    changed test so it succeeds

--- Tester.java
+++ Tester.java
@@ -4,6 +4,6 @@ import static org.junit.Assert.*;
 public class Tester{
 	@Test
 	public void test(){
-		assertEquals(7, Multiplication.multiply(2,4));
+		assertEquals(8, Multiplication.multiply(2,4));
 	}
 }
